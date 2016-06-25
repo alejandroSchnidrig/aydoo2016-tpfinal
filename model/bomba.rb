19da31826d8e106930859c78b3fab9768c6536c3
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-06-25 23:01:13 +0000

    se crea clase bomba, debria ser 100 la vida al crearse

---
+++ model/bomba.rb
@@ -0,0 +1,9 @@
+require_relative '../model/objeto_espacial'
+
+class Bomba < ObjetoEspacial
+
+  def initialize(vida = 100, masa = 100)
+    super
+  end
+
+end
---
+++ spec/bomba_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require 'spec_helper'
+require_relative '../model/bomba'
+
+describe 'bomba' do
+
+  it 'deberia ser 100 la vida al momento de su creacion' do
+    bomba = Bomba.new
+    expect(bomba.vida).to eq(100)
+  end
+
+end
