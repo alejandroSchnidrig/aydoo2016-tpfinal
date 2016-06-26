eaf47e4d9feac4080df9966e45eddc8ee888ca69
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-06-26 20:51:26 +0000

    agregado mapa de efectos al objeto nave

--- model/nave.rb
+++ model/nave.rb
@@ -1,11 +1,24 @@
 require_relative '../model/objeto_espacial'
 require_relative '../model/efecto_destructivo'
+require_relative '../model/efecto_masa'
+require_relative '../model/efecto_constructivo'
+require_relative '../model/misil'
+require_relative '../model/bomba'
+require_relative '../model/asteroide'
+require_relative '../model/estrella'
+
 
 class Nave < ObjetoEspacial
 
   def initialize(vida = 100, masa = 100)
     super
-    @efectos = {Nave => EfectoDestructivo.new(100)}	
+    @efectos = {
+      Nave => EfectoDestructivo.new(100),
+      Misil => EfectoDestructivo.new(80),
+      Bomba => EfectoDestructivo.new(50),
+      Asteroide => EfectoMasa.new(-50),
+      Estrella => EfectoConstructivo.new
+    }
   end
 
 end
