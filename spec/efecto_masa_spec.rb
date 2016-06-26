9bd1c544a42177330e4dc2fe7a424ebda959944d
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-06-26 18:28:36 +0000

    deberia destruirse el elemento

--- spec/efecto_masa_spec.rb
+++ spec/efecto_masa_spec.rb
@@ -17,12 +17,20 @@ describe 'efecto_masa' do
    expect(nave.masa).to eq(50)
   end
   
-   it 'deberia modificar la masa del asteroide al chocar con una nave correctemente' do
+  it 'deberia modificar la masa del asteroide al chocar con una nave correctemente' do
    efecto = EfectoMasa.new(10)
    nave = Nave.new
    asteroide = Asteroide.new
    efecto.aplicar_efecto(asteroide, nave)
    expect(asteroide.masa).to eq(110)
+  end
+
+  it 'deberia destruir al elemento al tener masa negativa o cero' do
+   efecto = EfectoMasa.new(-100)
+   nave = Nave.new
+   asteroide = Asteroide.new
+   efecto.aplicar_efecto(nave, asteroide)
+   expect(nave.objeto_destruido?).to eq(true)
   end
 
 end
