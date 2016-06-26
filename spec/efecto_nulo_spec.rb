f4f2dc0c1afcd93a15f92e0469f31ce0cfc7242f
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-06-26 15:30:55 +0000

    la nave no cambia su estado

--- spec/efecto_nulo_spec.rb
+++ spec/efecto_nulo_spec.rb
@@ -5,17 +5,24 @@ require_relative '../model/efecto_nulo'
 describe 'efecto_destructivo' do
 
   it 'deberia tener la misma vida la nave' do
-    efecto = EfectoNulo.new(0)
+    efecto = EfectoNulo.new(50)
     nave = Nave.new
     efecto.aplicar_efecto(nave, nil)
     expect(nave.vida).to eq(100)
   end
 
   it 'deberia tener la misma masa la nave' do
-    efecto = EfectoNulo.new(0)
+    efecto = EfectoNulo.new(50)
     nave = Nave.new
     efecto.aplicar_efecto(nave, nil)
     expect(nave.masa).to eq(100)
+  end
+
+  it 'deberia estar viva la nave' do
+    efecto = EfectoNulo.new(200)
+    nave = Nave.new
+    efecto.aplicar_efecto(nave, nil)
+    expect(nave.objeto_destruido?).to eq(false)
   end
 
 end
