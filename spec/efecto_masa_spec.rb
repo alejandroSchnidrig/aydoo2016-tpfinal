5614cd566c05b270fd7f9368518cad1ad39e8252
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-06-26 16:23:49 +0000

    efecto masa deberia funcionar correctamente

--- spec/efecto_masa_spec.rb
+++ spec/efecto_masa_spec.rb
@@ -9,4 +9,13 @@ describe 'efecto_masa' do
     expect(efecto.masa).to eq(50)
   end
 
+  it 'deberia modificar la masa de la nave al chocar con un asteroide correctemente' do
+   efecto = EfectoMasa.new(-50)
+   nave = Nave.new
+   asteroide = Asteroide.new
+   asteroide.masa=50
+   efecto.aplicar_efecto(nave, asteroide)
+   expect(nave.masa).to eq(75)
+  end
+
 end
