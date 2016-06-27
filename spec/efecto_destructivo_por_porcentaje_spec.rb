f8982107ecfbf80d9bab17dbe64b1463e2971925
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-06-27 03:17:49 +0000

    deberia destruise la estrella al chocar con una nave

--- spec/efecto_destructivo_por_porcentaje_spec.rb
+++ spec/efecto_destructivo_por_porcentaje_spec.rb
@@ -9,4 +9,12 @@ describe 'efecto_destructivo_por_porcentaje' do
     expect(efecto.vida).to eq(100)
   end
 
+  it 'deberia destruir a la estrella al chocar con una nave' do
+    efecto = EfectoDestructivoPorPorcentaje.new(100)
+    nave = Nave.new
+    estrella = Estrella.new		
+    efecto.aplicar_efecto(estrella, nave)
+    expect(estrella.vida).to eq(0)
+  end
+
 end
